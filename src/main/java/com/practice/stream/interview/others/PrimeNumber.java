9d0434f1aba8c2e4127c265adacb7e70d7416ca7
prathameshm1 <prathameshm@touras.co>
2026-04-09 12:30:42 +0530

    Added

---
+++ src/main/java/com/practice/stream/interview/others/PrimeNumber.java
@@ -0,0 +1,15 @@
+package com.practice.stream.interview.others;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+
+		int n = 19;
+
+		for (int i = 0; i < n / 2; i++) {
+			if (i % 2 == 0) {
+				
+			}
+		}
+	}
+}
